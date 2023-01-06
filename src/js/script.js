70dfc5ffb8da32700d7e96a4c995347a36906756
Kateryna <katepysova97@gmail.com>
2023-01-06 14:44:08 +0100

    Created header

--- src/js/script.js
+++ src/js/script.js
@@ -3,3 +3,15 @@ const themeSwitchBtn = document.querySelector(".theme-switcher");
 themeSwitchBtn.addEventListener("change", () => {
   document.body.classList.toggle("light-mode");
 });
+
+const headerCollapseBtn = document.querySelector(".menu-btn");
+const header = document.querySelector(".header");
+headerCollapseBtn.addEventListener("click", () => {
+  header.classList.toggle("collapsed");
+});
+
+window.addEventListener("resize", () => {
+  if (header.classList.contains("collapsed")) {
+    header.classList.toggle("collapsed");
+  }
+});
